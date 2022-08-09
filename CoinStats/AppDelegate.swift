e989418e5e0ca9810ed4e86660b4ea44282f2c36
Grigor Chapkinyan <grigorchapkinyan@Grigors-MacBook-Pro.local>
2022-08-09 21:13:08 +0400

    first commit

---
+++ CoinStats/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  CoinStats
+//
+//  Created by Grigor Chapkinyan on 09.08.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: - Private Properties
+    
+    var window: UIWindow?
+
+    // MARK: - App Life Cycle
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
